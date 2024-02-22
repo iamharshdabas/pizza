2479291e728478cec147fa80f318cd300ae4bb49
Harsh Dabas <iamharshdabas@gmail.com>
2024-02-22 14:56:06 +0530

    feat: add AppLayout and Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import CartPage from './pages/CartPage'
+import HomePage from './pages/HomePage'
+import MenuPage from './pages/MenuPage'
+import OrderPage from './pages/OrderPage'
+import NewOrderPage from './pages/NewOrderPage'
+import AppLayout from './containers/AppLayout'
 
 const router = createBrowserRouter([
-  { path: '/' },
-  { path: '/menu' },
-  { path: '/cart' },
-  { path: '/order/new' },
-  { path: '/order/:orderID' },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/menu', element: <MenuPage /> },
+      { path: '/cart', element: <CartPage /> },
+      { path: '/order/new', element: <NewOrderPage /> },
+      { path: '/order/:orderID', element: <OrderPage /> },
+    ],
+  },
 ])
 
 const App = () => {
