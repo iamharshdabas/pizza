a15807915215e4d0c06a3095f378861ec59c4a5c
Harsh Dabas <iamharshdabas@gmail.com>
2024-02-22 21:17:41 +0530

    feat: add header search bar for searching orders

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,9 +1,31 @@
+import { useNavigate } from 'react-router-dom'
 import NavBar from '../components/NavBar'
+import React, { useState } from 'react'
+import Input from '../components/Input'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const [query, setQuery] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    navigate(`/order/${query}`)
+  }
+
   return (
     <div>
       <NavBar />
+      <form onSubmit={handleSubmit}>
+        <Input
+          value={query}
+          onChange={handleChange}
+          placeholder="Search Order"
+        />
+      </form>
     </div>
   )
 }
