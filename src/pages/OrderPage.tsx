81a76e30ba4062238e8db24931ebaaa2944b5e95
Harsh Dabas <iamharshdabas@gmail.com>
2024-02-24 20:02:23 +0530

    chore: add OrderPage css

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -5,25 +5,38 @@ type orderProps = {
   data: OrderType
 }
 
+function formatOrderDeliveryTime(dateString: string) {
+  const deliveryDate = new Date(dateString)
+
+  const hours = deliveryDate.getUTCHours()
+  const minutes = deliveryDate.getUTCMinutes()
+
+  const formatTimeUnit = (value: number, unit: string) =>
+    value > 0 ? `${value} ${unit}` : ''
+  const formattedTime = `${formatTimeUnit(hours, 'hour')} ${formatTimeUnit(minutes, 'min')}`
+  return formattedTime.trim()
+}
+
 const OrderPage = () => {
   const order: orderProps = useLoaderData() as orderProps
   const data: OrderType = order.data
 
   return (
-    <>
-      <div>{data.id}</div>
-      <div>{data.customer}</div>
-      <div>{data.priority}</div>
-      <ul>
+    <div className="space-y-2 rounded-2xl bg-amber-950 p-4 font-mono">
+      <div className="text-xl uppercase">Name: {data.customer}</div>
+      <div className="text-xl uppercase">OrderId: {data.id}</div>
+      <ul className="flex gap-4">
+        Cart:
         {data.cart.map((pizza) => (
           <li key={pizza.pizzaId}>{pizza.name}</li>
         ))}
       </ul>
-      <div>{data.orderPrice}</div>
-      <div>{data.priorityPrice}</div>
-      <div>{data.status}</div>
-      <div>{data.estimatedDelivery}</div>
-    </>
+      <div>Priority: {data.priority ? 'YES' : 'NO'}</div>
+      <div>Priority Price: {data.priorityPrice}</div>
+      <div>Order Price: {data.orderPrice}</div>
+      <div>Status: {data.status}</div>
+      <div>Time: {formatOrderDeliveryTime(data.estimatedDelivery)}</div>
+    </div>
   )
 }
 
