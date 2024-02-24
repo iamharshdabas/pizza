7f880b788c07d3acf379cad7cfabc1db8e8299b5
Harsh Dabas <iamharshdabas@gmail.com>
2024-02-24 15:33:00 +0530

    chore: add Loading css

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,14 @@
+// https://codepen.io/Bilal1909/pen/pobgJae
 const Loading = () => {
-  return <div>Loading</div>
+  const bars = new Array(10).fill(0)
+
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-stone-900/40 backdrop-blur-md">
+      {bars.map(() => (
+        <div className="wave m-2 h-16 w-2 rounded-lg bg-amber-600"></div>
+      ))}
+    </div>
+  )
 }
 
 export default Loading
