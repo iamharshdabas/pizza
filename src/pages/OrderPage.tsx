89c2c7795cbfdb85f3fb6838e9a660271259dba8
Harsh Dabas <iamharshdabas@gmail.com>
2024-03-03 19:06:47 +0530

    feat: copy order id on click

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from 'react-router-dom'
 import OrderType from '../types/OrderType'
+import { useState } from 'react'
 
 type orderProps = {
   data: OrderType
@@ -18,17 +19,31 @@ function formatOrderDeliveryTime(dateString: string) {
 }
 
 const OrderPage = () => {
-  const order: orderProps = useLoaderData() as orderProps
-  const data: OrderType = order.data
+  const api: orderProps = useLoaderData() as orderProps
+  const order: OrderType = api.data
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(order.id).then(() => setIsCopied(!isCopied))
+  }
 
   return (
     <div className="mx-auto max-w-md rounded-2xl bg-stone-900 p-4">
-      <div className="text-xl font-bold">Hi, {data.customer}</div>
-      <div className="text-lg font-bold">Your order id is {data.id}</div>
+      <div className="text-xl font-bold">Hi, {order.customer}</div>
+      <div>
+        Your order id is{' '}
+        <button
+          onClick={handleCopy}
+          className="rounded-2xl bg-stone-800 px-4 py-1 font-mono text-lg font-bold tracking-widest shadow-xl"
+        >
+          {order.id}
+        </button>{' '}
+        {isCopied ? '(copied)' : '(click to copy)'}
+      </div>
       <div className="py-8">
         <p className="opacity-80">Your cart order contains</p>
         <ul className="flex flex-wrap gap-4">
-          {data.cart.map((pizza) => (
+          {order.cart.map((pizza) => (
             <li
               className="rounded-2xl bg-stone-800 px-4 py-1 text-lg shadow-lg"
               key={pizza.pizzaId}
@@ -38,20 +53,20 @@ const OrderPage = () => {
           ))}
         </ul>
       </div>
-      <div className="text-xl font-bold">Total Price: ${data.orderPrice}</div>
-      {data.priority && (
+      <div className="text-xl font-bold">Total Price: ${order.orderPrice}</div>
+      {order.priority && (
         <div className="text-lg opacity-90">
           Your order is Priotized at the price of{' '}
-          <span className="font-bold">${data.priorityPrice}</span>
+          <span className="font-bold">${order.priorityPrice}</span>
         </div>
       )}
       <div className="pt-4">
-        Your order is <span className="text-lg font-bold">{data.status}</span>
+        Your order is <span className="text-lg font-bold">{order.status}</span>
       </div>
       <div>
         Will be delivered at{' '}
         <span className="text-lg font-bold">
-          {formatOrderDeliveryTime(data.estimatedDelivery)}
+          {formatOrderDeliveryTime(order.estimatedDelivery)}
         </span>
       </div>
     </div>
